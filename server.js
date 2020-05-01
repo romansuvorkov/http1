c7312e7da66f971f7d261cd774ce8a71c283d08e
SuvorkovRN <suvorkovRN@gmail.com>
2020-05-02 00:55:22 +0500

    CORS back

--- server.js
+++ server.js
@@ -11,32 +11,32 @@ const server = http.createServer(app.callback()).listen(port);
 
 
 //Слайд 52 CORS
-// app.use(async (ctx, next) => {
-//     const origin = ctx.request.get('Origin');
-//     if (!origin) {
-//         return await next();
-//     }
-//     const headers = { 'Access-Control-Allow-Origin': '*', };
-//     if (ctx.request.method !== 'OPTIONS') {
-//         ctx.response.set({...headers});
-//         try {
-//             return await next();
-//         } catch (e) {
-//             e.headers = {...e.headers, ...headers};
-//             throw e;
-//         }
-//     }
-//     if (ctx.request.get('Access-Control-Request-Method')) {
-//         ctx.response.set({
-//             ...headers,
-//             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-//         });
-//         if (ctx.request.get('Access-Control-Request-Headers')) {
-//             ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-//         }
-//         ctx.response.status = 204; // No content
-//     }
-// });
+app.use(async (ctx, next) => {
+    const origin = ctx.request.get('Origin');
+    if (!origin) {
+        return await next();
+    }
+    const headers = { 'Access-Control-Allow-Origin': '*', };
+    if (ctx.request.method !== 'OPTIONS') {
+        ctx.response.set({...headers});
+        try {
+            return await next();
+        } catch (e) {
+            e.headers = {...e.headers, ...headers};
+            throw e;
+        }
+    }
+    if (ctx.request.get('Access-Control-Request-Method')) {
+        ctx.response.set({
+            ...headers,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+        });
+        if (ctx.request.get('Access-Control-Request-Headers')) {
+            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+        }
+        ctx.response.status = 204; // No content
+    }
+});
 
 app.use(koaBody({
     urlencoded: true,
@@ -57,36 +57,8 @@ class Ticket {
 
 
 
-app.use(async (ctx, next) => {
+app.use(async ctx => {
 
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-      return await next();
-  }
-  const headers = { 'Access-Control-Allow-Origin': '*', };
-  if (ctx.request.method !== 'OPTIONS') {
-      ctx.response.set({...headers});
-      try {
-          return await next();
-      } catch (e) {
-          e.headers = {...e.headers, ...headers};
-          throw e;
-      }
-  }
-  if (ctx.request.get('Access-Control-Request-Method')) {
-      ctx.response.set({
-          ...headers,
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-      });
-      if (ctx.request.get('Access-Control-Request-Headers')) {
-          ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-      }
-      ctx.response.status = 204; // No content
-  }
-
-
-
-  
 if (ctx.method === 'GET') {
     const { id } = ctx.request.query;
     if (id) {
